a3cd40afc2051cd25014cda784d245bbffa69777
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-07-23 00:50:18 +0900

    Python언어 BAEKJOON 2438번 문제 풀기!(1)

--- Python_2438_1.py
+++ Python_2438_1.py
@@ -10,7 +10,7 @@ N=int(input()) # 사용자로부터 정수 입력받기
 num=1 # num 선언 및 초기화
 
 for i in range(N): # i에 0부터 N-1까지 대입하며 반복
-    for j in range(num): # 
-        print("*", end="")
+    for j in range(num): # j에 0부터 num-1까지 대입하며 반복
+        print("*", end="") # 별 출력하기
     num+=1 # num에 num+1 대입하기
     print() # 한 줄 띄기
